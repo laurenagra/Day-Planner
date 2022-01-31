5e02de8fe6afe958dcf0d31add0cfb7482ea34f0
Lauren A <laurenhaileyama@gmail.com>
2022-01-31 11:22:09 -0500

    Introduce ready function? Create large function most of the JS is stored under

--- scipt.js
+++ scipt.js
@@ -1,26 +1,30 @@
 // GIVEN I am using a daily planner to create a schedule
 // WHEN I open the planner
 // THEN the current day is displayed at the top of the calendar
-//Use moment here 
-// Display today's day and date
-//one big function?
-//try implemeneting ready()
-var todayDate = moment().format('dddd, MMM Do YYYY');
-$("#currentDay").html(todayDate);
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 
 
 
+// if (past) { block < current hour } remove future and present class, add past class
+//else if (block hour === current hour){ remove future and past class, add present class} use $(this)
+//else  { remove past and present class and add future class colors are predetermined}
 
+$(document).ready(function(){//tell browser to load html and css first 
+    $("#currentDay").text(moment().format("MMM Do YYYY, hh:mm:ss a"));//moment.js
+    //assign event listener for save button applied to each time block
+    $(".saveBtn").on("click", function( ){
+        console.log(this);
+        //get nearby values in jquery
+        var text = $(this).siblings(".description").val(); // taken the change from the sibling html description attribute
+        var time = $(this).parent().attr("id"); // taken the change from the parent html id attribute
+   
+        //sets items in local storage
+        localStorage.setItem(text, time);
+    })
 
-
+})
 
 //if statement with time based criteria to set the colors
 // WHEN I click into a timeblock
-//Click event that opens a memo? Or a Card
 // THEN I can enter an event
 // WHEN I click the save button for that timeblock
 
