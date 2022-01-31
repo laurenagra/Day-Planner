86d39532af00836397a4447504af55d23c80879d
Lauren A <laurenhaileyama@gmail.com>
2022-01-31 10:25:38 -0500

    Update initial js using moment for time and the timeblocks in html

--- scipt.js
+++ scipt.js
@@ -1,13 +1,28 @@
 // GIVEN I am using a daily planner to create a schedule
 // WHEN I open the planner
 // THEN the current day is displayed at the top of the calendar
+//Use moment here 
+var timeDisplayEl = $('#time-display');
+
+
+function displayTime() {
+    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+    timeDisplayEl.text(rightNow);
+  }
 // WHEN I scroll down
 // THEN I am presented with timeblocks for standard business hours
 // WHEN I view the timeblocks for that day
 // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+//if statement with time based criteria to set the colors
 // WHEN I click into a timeblock
+//Click event that opens a memo? Or a Card
 // THEN I can enter an event
 // WHEN I click the save button for that timeblock
 // THEN the text for that event is saved in local storage
+//Append child and save to local storage 
 // WHEN I refresh the page
 // THEN the saved events persist
+
+
+
+setInterval(displayTime, 1000);
