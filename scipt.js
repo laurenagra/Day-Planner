fb6bbdf881378b99a2fd65a4e37df1c6a59ffb84
Lauren A <laurenhaileyama@gmail.com>
2022-01-31 11:53:01 -0500

    Update html and java function- save button does not yet work

--- scipt.js
+++ scipt.js
@@ -1,73 +1,60 @@
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
+$(document).ready(function () {
+  //tell browser to load html and css first
+  $("#currentDay").text(moment().format("MMM Do YYYY, hh:mm:ss a")); //moment.js
 
+  //assign event listener for save button applied to each time block
+  $(".saveBtn").on("click", function () {
+    console.log(this);
 
+    //get nearby values in jquery
+    var text = $(this).siblings(".description").val(); // taken the change from the sibling html description attribute
+    var time = $(this).parent().attr("id"); // taken the change from the parent html id attribute
 
-// if (past) { block < current hour } remove future and present class, add past class
-//else if (block hour === current hour){ remove future and past class, add present class} use $(this)
-//else  { remove past and present class and add future class colors are predetermined}
+    //sets items in local storage
+    localStorage.setItem(text, time);
+  });
 
-$(document).ready(function(){//tell browser to load html and css first 
-    $("#currentDay").text(moment().format("MMM Do YYYY, hh:mm:ss a"));//moment.js
-    //assign event listener for save button applied to each time block
-    $(".saveBtn").on("click", function( ){
-        console.log(this);
-        //get nearby values in jquery
-        var text = $(this).siblings(".description").val(); // taken the change from the sibling html description attribute
-        var time = $(this).parent().attr("id"); // taken the change from the parent html id attribute
-   
-        //sets items in local storage
-        localStorage.setItem(text, time);
-    })
-    function timeTracker(){
-        //current number of hours
-        var timeNow = moment().hour();
+  function timeTracker() {
+    //current number of hours
+    var timeNow = moment().hour();
 
-        //loop over timeblocks 
-        $(".time-block").each(function(){
-            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+    //loop over timeblocks
+    $(".time-block").each(function () {
+      var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
-            //check time and add classes for timeblock colors
-            if (blockTime < timeNow) {
-                $(this).removeClass("future");
-                $(this).removeClass("present");
-                $(this).addClass("past");
-            }
-            else if (blockTime === timeNow){
-                $(this).removeClass("future");
-                $(this).removeClass("past");
-                $(this).addClass("present");
-            } 
-            else {
-                $(this).removeClass("past");
-                $(this).removeClass("present");
-                $(this).addClass("future");
-            }
+      //check time and add classes for timeblock colors
+      if (blockTime < timeNow) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+      } else if (blockTime === timeNow) {
+        $(this).removeClass("future");
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
+  }
 
+  //Get items from local storage if any exist
+  $("#hour8 .description").val(localStorage.getItem("hour8"));
+  $("#hour9 .description").val(localStorage.getItem("hour9"));
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour13 .description").val(localStorage.getItem("hour13"));
+  $("#hour14 .description").val(localStorage.getItem("hour14"));
+  $("#hour15 .description").val(localStorage.getItem("hour15"));
+  $("#hour16 .description").val(localStorage.getItem("hour16"));
+  $("#hour17 .description").val(localStorage.getItem("hour17"));
 
-
-        })
-    }
-
-
-
-})
-
-//if statement with time based criteria to set the colors
-// WHEN I click into a timeblock
-// THEN I can enter an event
-// WHEN I click the save button for that timeblock
-
-
-
-
+  //Call the function to repeat
+  timeTracker();
+});
 
 // THEN the text for that event is saved in local storage
-//Append child and save to local storage 
 // WHEN I refresh the page
 // THEN the saved events persist
-
-
-
-setInterval(displayTime, 1000);
